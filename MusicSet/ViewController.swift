b68e44aab581cdd838fa63db5e1aa25ada4a1b31
110719008 <you@example.com>
2021-07-12 14:56:24 +0800

    Initial Commit

---
+++ MusicSet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicSet
+//
+//  Created by 林君翰 on 2021/7/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
